b3ad75ae914e14c9b09b419a68ef79bf0e505969
Fluffelluff <hopperbrando@gmail.com>
2023-09-20 13:01:08 +0100

    created a TestScript and used translation

---
+++ Unity Tutorial/Assets/Scripts/TestScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("you were supposed to bring balance to the force");
+        Debug.Log("Not plunge it into darkness!");
+
+        transform.Translate(20, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("No no no no no no no no!");
+    }
+}
