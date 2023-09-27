000f3ab9e6e47f35992b54fae942bd086b11c59b
Fluffelluff <hopperbrando@gmail.com>
2023-09-27 12:55:11 +0100

    Assets are added to Images, Added Variables to console logs

--- Unity Tutorial/Assets/Scripts/TestScript.cs
+++ Unity Tutorial/Assets/Scripts/TestScript.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    //string combinedString;
+
+    // editor variables
+    [SerializeField]
+
+    string editorString = "VALUE";
+
     // Start is called before the first frame update
     void Start()
     {
+        /*
         Debug.Log("you were supposed to bring balance to the force");
         Debug.Log("Not plunge it into darkness!");
 
         transform.Translate(20, 0, 0);
+        */
+
+        //Declating a variable calle firstString
+        // string firstString = "You shall not...";
+        //string secondString = " PASS!";
+
+        // string combinedString = firstString + secondString;
+
+        // Debug.Log(combinedString);
+
+        Debug.Log(editorString);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         Debug.Log("No no no no no no no no!");
-    }
+        */    
+
+        //Debug.Log(combinedString);
+        }
 }
