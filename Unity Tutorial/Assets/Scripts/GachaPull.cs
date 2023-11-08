6b6d405af728670ca39ebb37c6e24ad8ce890584
Fluffelluff <hopperbrando@gmail.com>
2023-11-08 10:02:03 +0000

    Adding a Gacha System to a new test object

---
+++ Unity Tutorial/Assets/Scripts/GachaPull.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GachaPull : MonoBehaviour
+{
+        // Constants
+        private float startingCurrency = 100.0f;  
+        private float pullCost = 10.0f;           
+        private float rewardChance = 0.2f;       
+
+        // Variables
+        private int numPulls = 0;
+        private float chanceRewardInverse = 0.0f;
+        private float overallRewardInverse = 0.0f;
+        private float overallRewardChance = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // Initialize numPulls to 0
+        numPulls = 0;
+
+        // Calculate numPulls based on startingCurrency and pullCost
+        numPulls = (int)(startingCurrency / pullCost);
+
+        // Display numPulls to the console
+        Debug.Log("numPulls: " + numPulls);
+
+        // Calculate chanceRewardInverse
+        chanceRewardInverse = 1.0f - rewardChance;
+
+        // Calculate overallRewardInverse using the power operator (^)
+        overallRewardInverse = Mathf.Pow(chanceRewardInverse, numPulls);
+
+        // Calculate overallRewardChance
+        overallRewardChance = 1.0f - overallRewardInverse;
+
+        // Display overallRewardChance to the console
+        Debug.Log("overallRewardChance: " + overallRewardChance);
+    }
+    }
+
+    // Update is called once per frame
+    //void Update()
+    
