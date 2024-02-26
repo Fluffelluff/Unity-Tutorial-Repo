4d8944d88f521952cbb32d3e401947223f3a0423
Fluffelluff <144230657+Fluffelluff@users.noreply.github.com>
2024-02-26 14:16:29 +0000

    Update UpdateScoreScript.cs

--- Unity Tutorial/Assets/Scripts/UpdateScoreScript.cs
+++ Unity Tutorial/Assets/Scripts/UpdateScoreScript.cs
@@ -1,15 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UpdateScoreScript : MonoBehaviour
 {
+
+
+    [SerializeField]
+    TMP_Text scoreDisplay = null;
+
+    private int currentScore = 0;
+
+    [SerializeField]
+    int debugAdd = 0;
+    //----------------------------------------------------------------
+    public int AddScore(int toAdd)
+    {
+        currentScore += toAdd;
+        scoreDisplay.text = currentScore.ToString();
+        return currentScore;
+
+    }
+    //----------------------------------------------------------------
+
+
     // Start is called before the first frame update
     void Start()
     {
-        int newScore = 0;
+        int newScore = AddScore(debugAdd);
+        Debug.Log("New Score is: " + newScore);
 
-        
+
     }
 
     // Update is called once per frame
